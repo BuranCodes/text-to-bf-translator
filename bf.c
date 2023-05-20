ca090d5dea337ab50aa0aeaa2151ba97cf7cf4db
BuranCodes <95971666+BuranCodes@users.noreply.github.com>
2023-05-20 16:00:39 +0200

    Big changes! Big improvements!

--- bf.c
+++ bf.c
@@ -2,36 +2,20 @@
  * Buran
  * String to Brainfuck translator, improved 2
  */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #define EXPANSION_FACTOR 1.5
 
+typedef unsigned char __uint8;
+
 int translation (const char *istr)
 {
-    FILE *fp;
-    // change omemsize if you want to
-    const unsigned int omemsize = 10256;
-    unsigned int ipos = 0;
-    // they never go above 255, we can keep it that way
-    unsigned char prevchar = 0;
-    unsigned char cell0, cell1, extra;
-    char *ostr = NULL;
-
-    if ((fp = fopen("output.bf", "w")) == NULL) {
-        fputs("Cannot open/write to file. Make sure the executable has the perm"
-        "issions to do so.\n", stderr);
-        exit(EXIT_FAILURE);
-    }
-
-    if ((ostr = (char *)calloc(omemsize, sizeof(char))) == NULL) {
-        // 10255 characters with one NUL termination maximum
-        fputs("Failed to allocate memory for output string.\n", stderr);
-        exit(EXIT_FAILURE);
-    }
-
-
+    __int32 ipos = 0;
+    /* they never go above 255, we can keep it that way */
+    __uint8 prevchar = 0;
+    __uint8 cell0, cell1, extra;
+    __uint8 start = 1;
     /* 
      * This algorithm checks for same characters, different characters where
      * the distance between is < 13 and dividing the character number in
@@ -42,43 +26,38 @@ int translation (const char *istr)
      * it would take for them the same prompt 434 and 245 characters,
      * respectively.
      */
-
     while(istr[ipos] != '\0') {
         int check = 0;
         // check for same char and input char
-        if (prevchar == istr[ipos] && ostr[(const int)strlen(ostr)-1] == '.') {
+        if (prevchar == istr[ipos]) {
             // print again
-            strcat(ostr, ".");
-            ipos++;
-            continue;
-        } else if (prevchar == istr[ipos] && ostr[(const int)strlen(ostr)-1] == '<') {
-            strcat(ostr, ">.");
+            putchar('.');
             ipos++;
             continue;
         }
         // check distance between previous char and incoming char
         if (prevchar - istr[ipos] < 13 && prevchar - istr[ipos] > 0) {
-            for (char i = 0; i < prevchar - istr[ipos]; i++) {
-                strcat(ostr, "-");
+            for (__uint8 i = 0; i < prevchar - istr[ipos]; i++) {
+                putchar('-');
             }
-            strcat(ostr, ".");
+            putchar('.');
             prevchar = istr[ipos];
             ipos++;
             continue;
         } else if (istr[ipos] - prevchar < 13 && istr[ipos] - prevchar > 0) {
-            for (char i = 0; i < istr[ipos] - prevchar; i++) {
-                strcat(ostr, "+");
+            for (__uint8 i = 0; i < istr[ipos] - prevchar; i++) {
+                putchar('+');
             }
-            strcat(ostr, ".");
+            putchar('.');
             prevchar = istr[ipos];
             ipos++;
             continue;
         }
         /* 
-         * fall through if none works above
-         * calculation 
-         */
-        for (char i = 10; i > 1; i--) { // composite numbers
+        * fall through if none works above
+        * calculation 
+        */
+        for (__uint8 i = 10; i > 1; i--) { // composite numbers
             if (prevchar > istr[ipos] && (prevchar - istr[ipos]) % i == 0) {
                 cell0 = (prevchar - istr[ipos]) / i;
                 cell1 = i;
@@ -93,7 +72,7 @@ int translation (const char *istr)
                 check = 1;
         }
         if (check == 1) { // prime numbers
-            for (char j = 10; j > 1; j--) {
+            for (__uint8 j = 10; j > 1; j--) {
                 if (prevchar > istr[ipos] && (prevchar - istr[ipos]) % j == 1) {
                     cell0 = (prevchar - istr[ipos]) / j;
                     cell1 = j;
@@ -108,56 +87,36 @@ int translation (const char *istr)
             }
         }
         // writing to output string
-        if (ostr[(const int)strlen(ostr)-1] != '<' && (int)ostr[0] != 0) {
+        if (start != 1) {
             // return to cell 0
-            strcat(ostr, "<");
+            putchar('<');
         }
-        for (char i = 0; i < cell0; i++) {
-            if (strlen(ostr) == 0) {
-                strcat(ostr, "+");
-                i++;
-            }
-            strcat(ostr, "+");
+        for (__uint8 i = 0; i < cell0; i++) {
+            putchar('+');
         }
-        
-        strcat(ostr, "[");
-        
-        strcat(ostr, ">");
-        
+        fputs("[>", stdout);
         if (prevchar > istr[ipos]) { 
             for (int i = 0; i < cell1; i++) {
-                
-                strcat(ostr, "-");
+                putchar('-');
             }
         } else if (prevchar < istr[ipos]) {
             for (int i = 0; i < cell1; i++) {
-                
-                strcat(ostr, "+");
+                putchar('+');
             }
         }
-        
-        strcat(ostr, "<");
-        strcat(ostr, "-");
-        strcat(ostr, "]");
-        strcat(ostr, ">");
+        fputs("<-]>", stdout);
         if (extra == 1) { // for prime numbers
             if (prevchar > istr[ipos]) {
-                
-                strcat(ostr, "-");
-            } else if (prevchar < istr[ipos]) {
-                
-                strcat(ostr, "+");
+                putchar('-');
+            } else if (prevchar < istr[ipos]) {                
+                putchar('+');
             }
         }
-        
-        strcat(ostr, ".");
+        putchar('.');
+        start = 0;
         prevchar = istr[ipos];
         ipos++;
     }
-    
-    fprintf(fp, "%s", ostr);
-    free(ostr);
-    fclose(fp);
     return EXIT_SUCCESS;
 }
 
@@ -190,10 +149,8 @@ int main(void)
         tmpc[0] = (char)tmp;
         strcat(input, tmpc);
     }
-
     translation(input);
     free(input);
-
-    fputs("The string has been translated into Brainfuck code in output.bf\nThe file should be where you ran the executable.", stdout);
+    fputs("\nThe text has been translated into Brainfuck code.\n", stdout);
     return EXIT_SUCCESS;
 }
